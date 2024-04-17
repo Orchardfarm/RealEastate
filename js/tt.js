2d1ab43cadfc29139912b1551ccea690b83f23b1
izoo0 <isaiahwainaina34@gmail.com>
2024-04-17 10:34:39 +0300

    new update

---
+++ js/tt.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+<div class="item">
+<div class="card py-2 bg-primary rounded">
+    <div class="row p-1">
+        <div class="col-lg-5">
+            <img src="images/electric.png" alt="dumbbell--v1"/>
+        </div>
+        <div class="col-lg-7 text-white">
+           Electric Fence
+        </div>
+    </div>
+</div>
+</div>
